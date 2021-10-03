f0f489374f665cfd626b4307d2287312f9366bcf
genie0806 <genie0806@gmail.com>
2021-10-04 00:27:13 +0900

    globalkey 사용

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,30 @@ class SnackBarTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SnackBar_Test'),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('스낵바 vs 스낵면+찬밥')),
-            );
-          },
-          child: Container(
-            child: Text(
-              'SnackBar Push',
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.cyan),
+    final _messangerKey = GlobalKey<ScaffoldMessengerState>();
+    return MaterialApp(
+      scaffoldMessengerKey: _messangerKey,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SnackBar_Test'),
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              _messangerKey.currentState.showSnackBar(
+                SnackBar(content: Text('스낵바 vs 스낵면+찬밥')),
+              );
+            },
+            child: Container(
+              child: Text(
+                'SnackBar Push',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.cyan),
+              ),
+              height: 40,
+              width: 80,
+              color: Colors.amber,
             ),
-            height: 40,
-            width: 80,
-            color: Colors.amber,
           ),
         ),
       ),
